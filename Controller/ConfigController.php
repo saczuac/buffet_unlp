f46f39d50fd8003600480a1c1bba401eca455c48
sacha <sspinelli@danaide.com.ar>
2016-09-28 15:06:46 -0300

    finalmente, arreglado

--- Controller/ConfigController.php
+++ Controller/ConfigController.php
@@ -7,9 +7,10 @@ use Model\Resource\ConfiguracionResource;
 class ConfigController {
 
    public function showConfig($app){
-    $configResource = ConfiguracionResource::getInstance();
-    echo $app->view->render( "config.twig",
-     array('tituloDescripcion' => ($configResource->get('tituloDescripcion')),
+      $app->applyHook('must.be.administrador');
+      $configResource = ConfiguracionResource::getInstance();
+      echo $app->view->render( "config.twig",
+       array('tituloDescripcion' => ($configResource->get('tituloDescripcion')),
       'infoDescripcion' => ($configResource->get('infoDescripcion')),
       'imgDescripcion' => ($configResource->get('imgDescripcion')),
       'tituloMenu' => ($configResource->get('tituloMenu')),
@@ -18,16 +19,20 @@ class ConfigController {
   }
 
   public function setPaginacion($app,$value) {
+    $app->applyHook('must.be.administrador');
     ConfiguracionResource::getInstance()->edit('paginacion',$value);
     echo $app->redirect('/config');
   }
     public function setTituloDescripcion($app,$value) {
+    $app->applyHook('must.be.administrador');
     ConfiguracionResource::getInstance()->edit('tituloDescripcion',$value);
   }
   public function setInfoDescripcion($app,$value) {
+    $app->applyHook('must.be.administrador');
     ConfiguracionResource::getInstance()->edit('infoDescripcion',$value);
   }
   public function setImgDescripcion($app) {
+    $app->applyHook('must.be.administrador');
   	if (isset($_FILES["myFileInfo"])) {
   	     $target_path = "img/";
 	        $target_path = $target_path . basename( $_FILES["myFileInfo"]['name']); move_uploaded_file($_FILES["myFileInfo"]['tmp_name'], $target_path);
@@ -35,18 +40,22 @@ class ConfigController {
     }
   }
   public function setDescripcion($app,$titulo,$descripcion) {
+    $app->applyHook('must.be.administrador');
   	$this->setTituloDescripcion($app,$titulo);
   	$this->setInfoDescripcion($app,$descripcion);
   	$this->setImgDescripcion($app);
     echo $app->redirect('/config');
   }
   public function setTituloMenu($app,$value) {
+    $app->applyHook('must.be.administrador');
     ConfiguracionResource::getInstance()->edit('tituloMenu',$value);
   }
   public function setInfoMenu($app,$value) {
+    $app->applyHook('must.be.administrador');
     ConfiguracionResource::getInstance()->edit('infoMenu',$value);
   }
     public function setImgMenu($app) {
+    $app->applyHook('must.be.administrador');
     if (isset($_FILES["myFileMenu"])) {
   	$target_path = "img/";
 	$target_path = $target_path .  basename( $_FILES["myFileMenu"]['name']); move_uploaded_file($_FILES["myFileMenu"]['tmp_name'], $target_path);
@@ -54,6 +63,7 @@ class ConfigController {
 	}
   }
   public function setMenu($app,$titulo,$descripcion) {
+    $app->applyHook('must.be.administrador');
   	$this->setTituloMenu($app,$titulo);
   	$this->setInfoMenu($app,$descripcion);
   	$this->setImgMenu($app);
