ca14203384d91de0ffd9825561041b5c729f3514
sacha <sspinelli@danaide.com.ar>
2016-09-29 13:08:42 -0300

    ubicacion resource ..

--- Controller/UsuarioController.php
+++ Controller/UsuarioController.php
@@ -3,17 +3,19 @@
 namespace Controller;
 use Model\Entity\Usuario;
 use Model\Resource\UsuarioResource;
+use Model\Entity\Ubicacion;
+use Model\Resource\UbicacionResource;
 
 class UsuarioController {
 
   public function listUsuarios($app){
     $app->applyHook('must.be.administrador');
-    echo $app->view->render( "usuarios/index.twig", array('usuarios' => (UsuarioResource::getInstance()->get())));
+    echo $app->view->render( "usuarios/index.twig", array('usuarios' => (UsuarioResource::getInstance()->get()), 'ubicaciones' => (UbicacionResource::getInstance()->get())));
   }
 
-  public function newUsuario($app,$user,$pass,$nombre,$apellido,$documento,$telefono,$rol_id,$email,$ubicacion ) {
+  public function newUsuario($app,$user,$pass,$nombre,$apellido,$documento,$telefono,$rol_id,$email,$ubicacion_id = null) {
     $app->applyHook('must.be.administrador');
-    if (UsuarioResource::getInstance()->insert($user,$pass,$nombre,$apellido,$documento,$telefono,$rol_id,$email,$ubicacion)){
+    if (UsuarioResource::getInstance()->insert($user,$pass,$nombre,$apellido,$documento,$telefono,$rol_id,$email,$ubicacion_id)){
        $app->flash('success', 'El usuario ha sido dado de alta exitosamente');
     } else {
       $app->flash('error', 'No se pudo dar de alta el usuario');
@@ -21,8 +23,8 @@ class UsuarioController {
     echo $app->redirect('/usuarios');
   }
 
-  public function registrarUsuario($app,$user,$pass,$nombre,$apellido,$documento,$telefono,$rol_id = 2,$email,$ubicacion ) {
-    if (UsuarioResource::getInstance()->insert($user,$pass,$nombre,$apellido,$documento,$telefono,$rol_id,$email,$ubicacion)){
+  public function registrarUsuario($app,$user,$pass,$nombre,$apellido,$documento,$telefono,$rol_id = 2,$email,$ubicacion_id ) {
+    if (UsuarioResource::getInstance()->insert($user,$pass,$nombre,$apellido,$documento,$telefono,$rol_id,$email,$ubicacion_id)){
        $app->flash('success', 'El registro se ha realizado con éxito');
     } else {
       $app->flash('error', 'No se pudo dar de alta el usuario');
