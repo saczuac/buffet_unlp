203297a55e12f0d587c50bdc961ba3041a2702f6
sacha.bba <sacha.bba@gmail.com>
2016-10-20 16:45:25 -0300

    prueba bot 13

---
+++ BotController.php
@@ -0,0 +1,71 @@
+<?php
+namespace Controller;
+
+require_once 'vendor/autoload.php';
+use Slim\Slim;
+use Chatwork\JsonRequestMiddleware;
+
+$app = new Slim(array(
+  'debug' => true,
+));
+
+$app->add(new JsonRequestMiddleware());
+
+$url = 'https://api.telegram.org/bot296497556:AAFlvyDLjO921sqBVHhpTaV1W5D5GoUFRUw/sendMessage';
+$msg = array();
+$msg['disable_web_page_preview'] = true;
+$msg['reply_markup'] = null;
+
+$app->post('/hoy', function() use ($app) {
+    $idchat = $app->json_body['message']['chat']['id'];
+    $msg['chat_id'] = $idchat;
+    $msg['text']  = 'El menú del día es ensalada tropical';
+    $msg['reply_to_message_id'] = $app->json_body['message']['message_id'];
+    $options = array(
+      'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($msg)
+      )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+});
+
+$app->post('/maniana', function() use ($app) {
+  $idchat = $app->json_body['message']['chat']['id'];
+  $msg['chat_id'] = $idchat;
+  $msg['text']  = 'El menú de mañana es pollo';
+  $msg['reply_to_message_id'] = $app->json_body['message']['message_id'];
+  $options = array(
+    'http' => array(
+      'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($msg)
+    )
+  );
+  $context  = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+});
+
+$app->post('/help', function() use ($app) {
+  $idchat = $app->json_body['message']['chat']['id'];
+  $msg['chat_id'] = $idchat;
+  $msg['text']  = 'Los comandos disponibles son estos:' . PHP_EOL;
+  $msg['text'] .= '/start Inicializa el bot';
+  $msg['text'] .= '/menú Muestra el menú del día';
+  $msg['text'] .= '/help Muestra esta ayuda';
+  $msg['reply_to_message_id'] = $app->json_body['message']['message_id'];
+  $options = array(
+    'http' => array(
+      'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($msg)
+    )
+  );
+  $context  = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+});
+
+$app->run();
+ ?>
