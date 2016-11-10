89ac123462c0def0876e6c4d471c06f977f64b6b
sebaesg <sebastian_sanchezg@hotmail.com>
2016-11-10 13:02:21 -0300

    agrego el where

--- Model/Resource/PedidoResource.php
+++ Model/Resource/PedidoResource.php
@@ -111,7 +111,7 @@ class PedidoResource extends AbstractResource {
         $query_string = "
             SELECT sum(pr.precio_venta_unitario * i.cantidad) as y, p.fecha_alta as name
             FROM Model\Entity\PedidoDetalle i join i.pedido_id p join i.producto_id pr  
-            WHERE p.fecha_alta between :desde AND :hasta
+            WHERE p.estado_id='2' AND p.fecha_alta between :desde AND :hasta
             GROUP BY p.fecha_alta
             ORDER by p.fecha_alta";
 
