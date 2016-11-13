5b6548045af241233522a8d4b366b85b0dbd30db
sebaesg <sebastian_sanchezg@hotmail.com>
2016-11-13 10:07:29 -0300

    modificaciones

--- Model/Resource/PedidoResource.php
+++ Model/Resource/PedidoResource.php
@@ -118,7 +118,7 @@ class PedidoResource extends AbstractResource {
       $error=0;
       $pedido=$this->get($id);
       foreach ($pedido->getDetalles() as $detalle) {
-        if (intval($detalle->getCantidad() < ProductoResource::getInstance()->get($detalle->getProducto_Id())->getStock())) {
+        if (intval($detalle->getCantidad()) < ProductoResource::getInstance()->get($detalle->getProducto_Id())->getStock()) {
         }else{
           $error=1;
           break;
